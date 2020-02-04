4550111cc8c0b86258c6f20d00ee26f9ed0548e8
Michael Wells <mwells1286@gmail.com>
2020-02-04 11:19:16 -0800

    add changes

--- lib/game.rb
+++ lib/game.rb
@@ -13,21 +13,14 @@ class VimGame < (Example rescue Gosu::Window)
     super WIDTH, HEIGHT
 
     self.caption = "PacVim"
-    @player = 
+    @player =
     @sky = Gosu::Image.new("media/space.png", tileable: true)
     @map = Map.new("media/levelone.txt")
      end
 
- # def update
-  #  @player.move_left   if @window.button_down? KbH
-   # @player.move_right  if @window.button_down? KbL
-   # @player.move_up     if @window.button_down? KbK
-   # @player.move_down   if @window.button_down? KbJ
-  # end
 
   def draw
       @map.draw
   end
 
 end
-
