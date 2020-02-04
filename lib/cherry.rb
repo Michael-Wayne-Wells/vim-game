1fa6280d69e183a72e72dded73227f36076882ab
Student <example@example.com>
2020-02-04 14:14:16 -0800

    add dot, superdot and cherry classes

---
+++ lib/cherry.rb
@@ -0,0 +1,20 @@
+class Cherry
+  attr_reader :x, :y
+
+  def initialize(window, column, row)
+    @window = window
+    @image = Image.new(@window, "media/cherry.png", true)
+    @width = @image.width
+    @height = @image.height
+    @offset_y = -65
+    @x = column * @image.width
+    @y = @offset_y + row * 75
+  end
+
+  def update
+  end
+
+  def draw
+    @image.draw (@x, @y, 0)
+  end
+end
