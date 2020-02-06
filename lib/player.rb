3793915ace48bb59726bd967fb3f2bd5e17465b2
Student <example@example.com>
2020-02-06 10:59:39 -0800

    added grabbing dots and cherries

--- lib/player.rb
+++ lib/player.rb
@@ -76,8 +76,10 @@ class Player
   end
 
   def collect_dots(dots)
-    dots.reject! do |dot|
-      Gosu::distance(@x, @y, dot.x, dot.y) < 35
+    if @window.button_down? KbI
+      dots.reject! do |dot|
+        Gosu::distance(@x, @y, dot.x, dot.y) < 35
+      end
     end
   end
 
@@ -86,14 +88,16 @@ class Player
   end
 
   def collect_cherry(cherry)
-    if Gosu::distance(@x - 30, @y, cherry.x, cherry.y) < 80
-      puts 'cherry collected'
-      @success.play
-      @cherry_collected = true
-      true
-    else
-      false
-    end
+    if @window.button_down? KbI
+      if Gosu::distance(@x - 30, @y, cherry.x, cherry.y) < 80
+        puts 'cherry collected'
+        @success.play
+        @cherry_collected = true
+        true
+      else
+        false
+      end
+    end  
   end
 
 
